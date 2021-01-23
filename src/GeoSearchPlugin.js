8986f7fd521cba9b84a8ed3c7654f7725e1e1168
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-01-22 19:44:02 -0800

    Remove npmrc, use version ranges for dependencies

--- src/GeoSearchPlugin.js
+++ src/GeoSearchPlugin.js
@@ -2,6 +2,7 @@ import {withLeaflet, MapControl} from 'react-leaflet'
 import {Geosearch} from 'esri-leaflet-geocoder'
 
 class LeafletGeoSearch extends MapControl {
+  // eslint-disable-next-line class-methods-use-this
   createLeafletElement(props) {
     const GeoSearch = new Geosearch()
 
